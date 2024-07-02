32c667def645ec3659bc2b8fd766ae76853e99b5
Ricardo Gimenes Ferreira <ricardogi@google.com>
2024-07-02 16:45:16 -0300

    corrected validation errors

--- ec_form2.js
+++ ec_form2.js
@@ -1,42 +1,34 @@
 var g_ts_config = {
-  CSSEmail :'[aria-label="Correo Electrónico"]',
-  CSSPhoneNumber : '[aria-label="Teléfono"]',
+  CSSEmail :'[type=email]',
+  CSSPhoneNumber : '[type=tel]',
   country_code: '+57',
   CSSSubmitButton: '[type="submit"]',
   emailRegEx: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
   phoneRegEx: /^\+[1-9]\d{6,14}$/
- }
- g_ts_obj= g_ts_obj||{};
+ };
+ window.g_ts_obj = window.g_ts_obj||{};
  
  document.addEventListener('input',function(e){
   var input = e.target,
    isEmail = input.matches(g_ts_config.CSSEmail),
    isPhoneNumber = input.matches(g_ts_config.CSSPhoneNumber);
-  if (!isEmail && !isPhoneNumber) {
-    console.log(`TS alert: it is neither the E-mail nor the Phone number input;`)
-    return;
-  }
+  if (!isEmail && !isPhoneNumber) {return; /* Not the e-mail nor the Phonenumber input */}
   if (isEmail && g_ts_config.emailRegEx.test(input.value) ) {
    console.log( `TS alert: ${input.value} is a valid e-mail;` );
-   g_ts_obj.email = input.value
-  }
-  
-  
+   window.g_ts_obj.email = input.value
+  }  
   if (isPhoneNumber)  {
    var tel = '+' +  (g_ts_config.country_code + input.value).replace(/\D/g,'');
-      console.log(`TS alert: ${tel} is a valid phone Number;` );
+   console.log(`TS alert: ${tel} is a valid phone Number;` );
    if (! g_ts_config.phoneRegEx.test(tel)) return;
-   g_ts_obj.phone_number = tel;
+   window.g_ts_obj.phone_number = tel;
   }
  });
  
  document.addEventListener('click',  function(e){
   var  element = e.target;
-  if(!element.matches(CSSSubmitButton) && !CSSSubmitButton.closest(CSSSubmitButton)) {return;}
-  if(!g_ts_obj.phone_number && !g_ts_obj.email){
-    console.log(`TS alert: There is neither Phone number nor E-mail`);
-    return;
-  }
-  console.log(`TS alert: user-provided_data_event no DataLayer, use a variável Javacript: g_ts_obj (Email:${g_ts_obj.email}, Phone_number:${g_ts_obj.phone_number})`);
-  window.dataLayer.push({'event': 'user-provided_data_event'});
+  if(!element.matches(g_ts_config.CSSSubmitButton) && ! element.closest(g_ts_config.CSSSubmitButton)) {return;}
+  if(!window.g_ts_obj.phone_number && !window.g_ts_obj.email){return; /* not the submit button */}
+  console.log(`TS alert: user-provided_data_event no DataLayer, use a variável Javacript: window.g_ts_obj (Email:${window.g_ts_obj.email}, Phone_number:${window.g_ts_obj.phone_number})`);
+  window.dataLayer.push({'event': 'user_provided_data_event'});
  });
