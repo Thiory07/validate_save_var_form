2f4ac1662dee54ebced7c1271a00ee2667242da8
gimenesferreira <gimenesferreira@gmail.com>
2024-07-03 20:30:09 +0000

    added the old EC code for comparison

--- ec_form_button_click.js
+++ ec_form_button_click.js
@@ -1,65 +1,64 @@
 var g_ts_config = {
-    submitButtonCssSelector: '[type="submit"]',
-    emailCssSelector :'[type=email]',
-    phoneCssSelector : '[type=tel]',
-    country_code: '+57',
-    emailRegEx: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-    phoneRegEx: /^\+[1-9]\d{6,14}$/
-   };
+  submitButtonCssSelector: '[type="submit"]',
+  emailCssSelector :'[type=email]',
+  phoneCssSelector : '[type=tel]',
+  country_code: '+57',
+  emailRegEx: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+  phoneRegEx: /^\+[1-9]\d{6,14}$/
+};
 // Javascript variable for GTM or gTag:
-   window.g_ts_obj = window.g_ts_obj||{};
-   
-   window.g_ts_config.savePhoneNumber = function(phoneNumberDomElement){
-    if (!phoneNumberDomElement) {
-        console.log(`TS alert: Phone number CSS selector either wrong or no phone number input on the page`);
-        return;
-    }
-    var phone_number = phoneNumberDomElement.value.replace(/\D/g,''),
-     country_code = g_ts_config.country_code.replace(/\D/g,'');
+window.g_ts_obj = window.g_ts_obj||{};
 
-    if (!phone_number.startsWith(country_code)){phone_number = country_code+phone_number;};
-    phone_number = `+${phone_number}`;
+window.g_ts_config.savePhoneNumber = function(phoneNumberDomElement){
+  if (!phoneNumberDomElement) {
+    console.log(`TS alert: Phone number CSS selector either wrong or no phone number input on the page`);
+    return;
+  }
+  var phone_number = phoneNumberDomElement.value.replace(/\D/g,''),
+  country_code = g_ts_config.country_code.replace(/\D/g,'');
 
-    if (!g_ts_config.phoneRegEx.test(phone_number)){ console.log(`TS alert: the phone (${phone_number}) is not valid`);return;}
+  if (!phone_number.startsWith(country_code)){phone_number = country_code+phone_number;};
+  phone_number = `+${phone_number}`;
 
-    console.log(`TS info: The phone_number saved to Javascript variable window.g_ts_obj is (${phone_number});`);
-    window.g_ts_obj.phone_number = phone_number;
-   }
-   window.g_ts_config.saveEmail = function(emailDomElement){
-    if (!emailDomElement){
-        console.log(`TS alert: Email CSS selector either wrong or no Email input on the page`);
-        return;
-    }
-    var email = emailDomElement.value.trim();
-    if(!g_ts_config.emailRegEx.test(email)){
-        console.log(`TS alert: the email (${email}) is not valid`);
-        return;
-    }
-    console.log(`TS info: The email saved to Javascript (window.g_ts_obj) Variable is (${email});`);
-    window.g_ts_obj.email = email;
-   }
+  if (!g_ts_config.phoneRegEx.test(phone_number)){ console.log(`TS alert: the phone (${phone_number}) is not valid`);return;}
 
-   document.addEventListener('click',  function(e){
-    var element = e.target;
-    
-    /* not the submit button */
-    if(!element.matches(g_ts_config.submitButtonCssSelector) && ! element.closest(g_ts_config.submitButtonCssSelector)) {return;}
+  console.log(`TS info: The phone_number saved to Javascript variable window.g_ts_obj is (${phone_number});`);
+  window.g_ts_obj.phone_number = phone_number;
+}
+window.g_ts_config.saveEmail = function(emailDomElement){
+  if (!emailDomElement){
+      console.log(`TS alert: Email CSS selector either wrong or no Email input on the page`);
+      return;
+  }
+  var email = emailDomElement.value.trim();
+  if(!g_ts_config.emailRegEx.test(email)){
+      console.log(`TS alert: the email (${email}) is not valid`);
+      return;
+  }
+  console.log(`TS info: The email saved to Javascript (window.g_ts_obj) Variable is (${email});`);
+  window.g_ts_obj.email = email;
+}
 
-    var form = element.closest('form');
-    if (!form){ console.log(`TS warning: No parent form found`); }
+document.addEventListener('click',  function(e){
+var element = e.target;
 
-    var emailDomElement = form.querySelector(g_ts_config.emailCssSelector);
-    g_ts_config.saveEmail(emailDomElement);
+/* not the submit button */
+if(!element.matches(g_ts_config.submitButtonCssSelector) && ! element.closest(g_ts_config.submitButtonCssSelector)) {return;}
 
-    var phoneNumberDomElement = form.querySelector(g_ts_config.phoneCssSelector);
-    g_ts_config.savePhoneNumber(phoneNumberDomElement);
+var form = element.closest('form');
+if (!form){ console.log(`TS warning: No parent form found`); }
 
-    if(!window.g_ts_obj.phone_number && !window.g_ts_obj.email){
-        console.log(`TS alert: the phone_number (${window.g_ts_obj.phone_number}) and the email (${window.g_ts_obj.email})`);
-        return; /* No Email and no  Phone number on the form*/ 
-    }
-    console.log(`TS alert: user-provided_data_event no DataLayer, use a variável Javacript: window.g_ts_obj (Email:${window.g_ts_obj.email}, Phone_number:${window.g_ts_obj.phone_number})`);
-    window.dataLayer.push({'event': 'user_provided_data_event'});
-   });
+var emailDomElement = form.querySelector(g_ts_config.emailCssSelector);
+g_ts_config.saveEmail(emailDomElement);
 
-   
+var phoneNumberDomElement = form.querySelector(g_ts_config.phoneCssSelector);
+g_ts_config.savePhoneNumber(phoneNumberDomElement);
+
+if(!window.g_ts_obj.phone_number && !window.g_ts_obj.email){
+    console.log(`TS alert: the phone_number (${window.g_ts_obj.phone_number}) and the email (${window.g_ts_obj.email})`);
+    return; /* No Email and no  Phone number on the form*/ 
+}
+console.log(`TS alert: user-provided_data_event no DataLayer, use a variável Javacript: window.g_ts_obj (Email:${window.g_ts_obj.email}, Phone_number:${window.g_ts_obj.phone_number})`);
+window.dataLayer.push({'event': 'user_provided_data_event'});
+});
+
---
+++ ec_form_classic.js
@@ -0,0 +1,42 @@
+var g_EC = {
+    "email" : "[type='email']",
+    "phone_number" : "[type='tel']"
+  };
+
+  window.g_setupEC = Object.create(null);
+  window.g_ECObj = Object.create(null);
+
+  var g_countryCode = '55';
+
+  document.addEventListener('input', g_setup_ECObj);
+
+  function g_setup_ECObj(e) {
+    var input = e.target;
+    for(i in g_EC) {
+      if(input.matches(g_EC[i])) g_setupEC['g_' + i] = input.value;
+    }
+
+    g_save_toECObj();
+  }
+
+  function g_save_toECObj() {
+    for(i in g_EC) {
+      if(g_setupEC['g_' + i] && i === 'email' && g_validateMail(g_setupEC['g_' + i])) window.g_ECObj[i] = g_setupEC['g_' + i];
+      if(g_setupEC['g_' + i] && i === 'phone_number') {
+        var cleanedPhone = g_validatePhone(g_setupEC['g_' + i]);
+        var finalPhone = cleanedPhone.includes('+') ? cleanedPhone : cleanedPhone.startsWith(g_countryCode) ? 'g_' + cleanedPhone : '+' + g_countryCode + cleanedPhone;
+
+        finalPhone.length >= 11 && finalPhone.length <= 15 ? (window.g_ECObj[i] = finalPhone) : delete window.g_ECObj[i];
+      }
+    }
+  }
+
+  function g_validateMail(email) {
+    return /\S+@\S+\.\S+/.test(email);
+  }
+
+  function g_validatePhone(tel) {
+    return tel.replace(/\D/g, '');
+  }
+
+  g_save_toECObj();
