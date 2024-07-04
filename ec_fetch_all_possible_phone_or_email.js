1f5b25070142cf81e10d79378590e8db8792ea74
gimenesferreira <gimenesferreira@gmail.com>
2024-07-04 21:21:40 +0000

    created page scraper for emails

--- ec_fetch_all_possible_phone_or_email.js
+++ ec_fetch_all_possible_phone_or_email.js
@@ -8,38 +8,52 @@ var g_ts_config = {
 window.enhanced_conversion_data = window.enhanced_conversion_data || {};
 window.dataLayer = window.dataLayer || [];
 window.g_ts_config.country_code = g_ts_config.country_code.replace(/\D/g, "");
-window.windowHost = window.location.host.replace('www.','');
+window.windowHost = window.location.host.replace('www.', '');
 document.addEventListener('click', function (e) {
   var el = e.target;
   /* Not the submit button */
   if (!el.matches(g_ts_config.submitButtonCssSelector) && !el.closest(g_ts_config.submitButtonCssSelector)) { return; }
 
   var allInputs = document.querySelectorAll('input'), emails = [], phones = [];
-  allInputs.forEach(function (el){
+  allInputs.forEach(function (el) {
     var temEmail = el.value.trim();
-    if (g_ts_config.emailRegEx.test(temEmail) ) emails.push(temEmail);
+    if (g_ts_config.emailRegEx.test(temEmail)) emails.push(temEmail);
 
-    var tempPhone = el.value.replace(/\D/g,'');
+    var tempPhone = el.value.replace(/\D/g, '');
     tempPhone = tempPhone.startsWith(g_ts_config.country_code) ? `+${tempPhone}` : `+${g_ts_config.country_code}${tempPhone}`;
-    if(g_ts_config.phoneRegEx.test(tempPhone)) phones.push(tempPhone);
+    if (g_ts_config.phoneRegEx.test(tempPhone)) phones.push(tempPhone);
   });
-  
+
   if (emails.length > 0) window.enhanced_conversion_data.email = emails.length > 1 ? emails : emails[0];
   if (phones.length > 0) window.enhanced_conversion_data.phone_number = phones.length > 1 ? phones : phones[0];
-  
-  if (Object.keys(window.enhanced_conversion_data).length > 0){
-    console.log( window.enhanced_conversion_data);
-    if ( typeof gtag != 'undefined' ) { gtag("set", "user_data", g_ts_obj); }
-    window.dataLayer = window.dataLayer || [];
-    window.dataLayer.push({'event': 'upd_event'});
-  }
 
+  window.g_ts_config.fireUpdEvent();
 });
 
-
-  window.g_ts_config.extractEmailFromBody = function(){
-    var str =  document.body.textContent.toLocaleLowerCase();
-    var email = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b/g;
-    var tempEmails = str.match(email);
-    return tempEmails.filter(function (el) { return !el.includes(window.windowHost)});
+window.g_ts_config.fireUpdEvent = function(){
+  if (Object.keys(window.enhanced_conversion_data).length > 0) {
+    console.log(window.enhanced_conversion_data);
+    if (typeof gtag != 'undefined') { gtag("set", "user_data", g_ts_obj); }
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({ 'event': 'upd_event' });
   }
+}
+window.g_ts_config.extractEmailFromBody = function () {
+  var str = document.body.textContent.toLocaleLowerCase();
+  var email = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b/g;
+  var tempEmails = str.match(email);
+  return tempEmails.filter(function (el) { return !el.includes(window.windowHost) });
+}
+
+window.g_ts_config.init = function(){
+  window.enhanced_conversion_data = window.enhanced_conversion_data || {};
+  if (window.enhanced_conversion_data.email) window.g_ts_config.fireUpdEvent();
+  var emails = window.g_ts_config.extractEmailFromBody();
+  if (emails.length > 0) window.enhanced_conversion_data.email = emails.length > 1 ? emails : emails[0];
+  if (window.enhanced_conversion_data.email) window.g_ts_config.fireUpdEvent();
+}
+if (/complete|interactive|loaded/.test(document.readyState)) {
+  window.g_ts_config.init();
+} else {
+  document.addEventListener('DOMContentLoaded', window.g_ts_config.init);
+}
