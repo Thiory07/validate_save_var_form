689e5c70fcc2967d90238c1304dd27862e92eeaa
Ricardo Gimenes Ferreira <ricardogi@google.com>
2024-07-29 13:57:03 -0300

    Code 2, removed string literals because GTM doesn't accept it

--- ec_form2.js
+++ ec_form2.js
@@ -1,8 +1,10 @@
 var g_ts_config = {
+  /* Configurations */
   CSSEmail :'[type=email]',
-  CSSPhoneNumber : '[type=tel]',
+  CSSPhoneNumber : '[name="#/properties/telefono"]',
   country_code: '+57',
   CSSSubmitButton: '[type="submit"]',
+  /* Regular expressions */
   emailRegEx: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
   phoneRegEx: /^\+[1-9]\d{6,14}$/
  };
@@ -14,12 +16,12 @@ var g_ts_config = {
    isPhoneNumber = input.matches(g_ts_config.CSSPhoneNumber);
   if (!isEmail && !isPhoneNumber) {return; /* Not the e-mail nor the Phonenumber input */}
   if (isEmail && g_ts_config.emailRegEx.test(input.value) ) {
-   console.log( `TS alert: ${input.value} is a valid e-mail;` );
+   console.log( 'TS alert: '+ input.value+' is a valid e-mail;' );
    window.g_ts_obj.email = input.value
   }  
   if (isPhoneNumber)  {
    var tel = '+' +  (g_ts_config.country_code + input.value).replace(/\D/g,'');
-   console.log(`TS alert: ${tel} is a valid phone Number;` );
+   console.log('TS alert: '+ tel+ ' is a valid phone Number;');
    if (! g_ts_config.phoneRegEx.test(tel)) return;
    window.g_ts_obj.phone_number = tel;
   }
@@ -27,8 +29,8 @@ var g_ts_config = {
  
  document.addEventListener('click',  function(e){
   var  element = e.target;
-  if(!element.matches(g_ts_config.CSSSubmitButton) && ! element.closest(g_ts_config.CSSSubmitButton)) {return;}
-  if(!window.g_ts_obj.phone_number && !window.g_ts_obj.email){return; /* not the submit button */}
-  console.log(`TS alert: user-provided_data_event no DataLayer, use a variável Javacript: window.g_ts_obj (Email:${window.g_ts_obj.email}, Phone_number:${window.g_ts_obj.phone_number})`);
+  if(!element.matches(g_ts_config.CSSSubmitButton) && ! element.closest(g_ts_config.CSSSubmitButton)) {return; /* not the submit button */}
+  if(!window.g_ts_obj.phone_number && !window.g_ts_obj.email){return; /* There is no E-mail nor Phone number */}
+  console.log('TS alert: user-provided_data_event on DataLayer, use The Javacript variable: window.g_ts_obj \n(Email:'+ window.g_ts_obj.email+ ', Phone_number:'+window.g_ts_obj.phone_number+')');
   window.dataLayer.push({'event': 'user_provided_data_event'});
  });
