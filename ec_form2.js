78093e1c06149724fe55f89eeba803abdcc633aa
gimenesferreira <gimenesferreira@gmail.com>
2024-09-23 11:53:25 +0000

    changed some comments and switched var declarations spots

--- ec_form2.js
+++ ec_form2.js
@@ -1,7 +1,7 @@
 var g_ts_config = {
   /* Configurations */
-  CSSEmail :'[type=email]', // email selectors
-  CSSPhoneNumber : '[placeholder="Teléfono*"]', // phone number selectors
+  CSSEmail :'[type=email],[name="email"]', // email selectors
+  CSSPhoneNumber : '[name="tel"]', // phone number selectors
   CSSCountryCode : '', // leave empty if there is no country code input
   CSSSubmitButton: '[type="submit"]', // a button that will generate "user_provided_data_event"
   country_code: '+55', // if there is no Phone input, hancode the country code
@@ -15,32 +15,26 @@ var g_ts_config = {
  
  document.addEventListener('input',function(e){
   var input = e.target,
-   isEmail = input.matches(g_ts_config.CSSEmail),
-   isPhoneNumber = input.matches(g_ts_config.CSSPhoneNumber);
-   let isCountryCode = false;
-   if (g_ts_config.CSSCountryCode !=''){
-    isCountryCode = input.matches(g_ts_config.CSSCountryCode);
-   }
-   g_ts_config.temp_cc = g_ts_config.temp_cc || g_ts_config.country_code;
+  isEmail = input.matches(g_ts_config.CSSEmail),
+  isPhoneNumber = input.matches(g_ts_config.CSSPhoneNumber);
+  let isCountryCode = false;
 
   if (!isEmail && !isPhoneNumber) {return; /* Not the e-mail nor the Phonenumber input */}
   if (isEmail && g_ts_config.emailRegEx.test(input.value) ) {
    console.log( 'TS alert: '+ input.value+' is a valid e-mail;' );
-   window.g_ts_obj.email = input.value;
-   window.g_ECObj.email = input.value;
+   window.g_ts_obj.email = window.g_ECObj.email = input.value;
    return;
   }  
 
-  if (isPhoneNumber)  {
-   g_ts_config.temp_cc == g_ts_config.temp_cc || g_ts_config.country_code ; 
+  if (isPhoneNumber)  { 
    var DOMCountryCode;
-   if (g_ts_config.CSSCountryCode) DOMCountryCode = document.querySelector(g_ts_config.CSSCountryCode);
+   if (g_ts_config.CSSCountryCode && g_ts_config.CSSCountryCode !='') DOMCountryCode = document.querySelector(g_ts_config.CSSCountryCode);
    if (DOMCountryCode) g_ts_config.temp_cc = DOMCountryCode.value.replace(/\D/g,'');
+   g_ts_config.temp_cc = g_ts_config.temp_cc || g_ts_config.country_code ;
    var tel = '+' +  (g_ts_config.temp_cc + '' + input.value).replace(/\D/g,'');
    console.log('TS alert: '+ tel+ ' is a valid phone Number;');
    if (! g_ts_config.phoneRegEx.test(tel)) return;
-   window.g_ts_obj.phone_number = tel;
-   window.g_ECObj.phone_number = tel;
+   window.g_ts_obj.phone_number = window.g_ECObj.phone_number = tel;
   }
  });
  
