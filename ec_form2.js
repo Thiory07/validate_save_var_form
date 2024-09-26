c3119ebe7cef2f1fdbfc15d6a69c6e1816de273a
gimenesferreira <gimenesferreira@gmail.com>
2024-09-26 15:54:47 +0000

    added comments in portugues

--- ec_form2.js
+++ ec_form2.js
@@ -1,14 +1,28 @@
+/* 
+Como usar:
+
+Configuração:
+- Coloque os seletores para os campos de Email , telefone e DDI (código telefonico do país)
+- Coloque o seletor do botão de enviar.
+- Se o site não tiver um campo de DDI, deixe o campo "CSSCountryCode" vazio e prencha o "country_code" com o código do país do site.
+
+Acionamentos:
+- este código cria um evento adicional de dados do usuário no dataLayer ou para GTAG.
+- a variável que guarda os dados do usuário na janela é "window.g_ts_obj" que pode ser utilizado para envio de dados para google tag manager ou gtag.
+*/
 var g_ts_config = {
   /* Configurations */
-  CSSEmail :'[type=email],[name="email"]', // email selectors
-  CSSPhoneNumber : '[name="tel"]', // phone number selectors
-  CSSCountryCode : '', // leave empty if there is no country code input
-  CSSSubmitButton: '[type="submit"]', // a button that will generate "user_provided_data_event"
-  country_code: '+55', // if there is no Phone input, hancode the country code
+  CSSEmail :'[type=email],[name="email"]', 
+  CSSPhoneNumber : '[name="tel"]', 
+  CSSCountryCode : '',  
+  CSSSubmitButton: '[type="submit"]', 
+  country_code: '+55', 
 
   /* Regular expressions */
   emailRegEx: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
   phoneRegEx: /^\+[1-9]\d{6,14}$/
+
+  
  };
  window.g_ts_obj = window.g_ts_obj||{}; // objeto para armazenar dados do usuário
  window.g_ECObj = window.g_ECObj||{};
@@ -32,8 +46,9 @@ var g_ts_config = {
    if (DOMCountryCode) g_ts_config.temp_cc = DOMCountryCode.value.replace(/\D/g,'');
    g_ts_config.temp_cc = g_ts_config.temp_cc || g_ts_config.country_code ;
    var tel = '+' +  (g_ts_config.temp_cc + '' + input.value).replace(/\D/g,'');
-   console.log('TS alert: '+ tel+ ' is a valid phone Number;');
    if (! g_ts_config.phoneRegEx.test(tel)) return;
+   
+   console.log('TS alert: '+ tel+ ' is a valid phone Number;');
    window.g_ts_obj.phone_number = window.g_ECObj.phone_number = tel;
   }
  });
@@ -45,4 +60,5 @@ var g_ts_config = {
   if(!window.g_ts_obj.phone_number && !window.g_ts_obj.email){return; /* There is no E-mail nor Phone number */}
   console.log('TS alert: user-provided_data_event on DataLayer, use The Javacript variable: window.g_ts_obj \n(Email:'+ window.g_ts_obj.email+ ', Phone_number:'+window.g_ts_obj.phone_number+')');
   window.dataLayer.push({'event': 'user_provided_data_event'});
+  if( gtag ) gtag('set', 'user_data', window.g_ts_obj);
  });
