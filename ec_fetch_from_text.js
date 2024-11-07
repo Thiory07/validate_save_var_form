e14b4f0ef1ce947071b13927e3b30c58eea3f822
gimenesferreira <gimenesferreira@gmail.com>
2024-11-07 16:36:38 +0000

    added timer and checks to avoid multiple events firing at the same time

--- ec_fetch_from_text.js
+++ ec_fetch_from_text.js
@@ -3,7 +3,8 @@ window.g_ts__pii = window.g_ts__pii || {};
 
 window.g_ts__config.client_domain = 'testes-do-tio.free.nf';
 window.g_ts__config.user_provided_data_event = 'upd_event';
-
+window.dataLayer = window.dataLayer||[];
+window.g_ts__config.is_fired = false;
 window.g_ts__app = {};
 window.g_ts__app.extractEmails = function (text) {
   return text.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)/gi);
@@ -22,22 +23,21 @@ window.g_ts__app.findEmailsStart = function(){
   emails = emails.filter(function (email){ return !email.includes( window.g_ts__config.client_domain); });
   window.g_ts__pii.email = emails[0];
   console.log(window.g_ts__pii.email);
-  if (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(window.g_ts__pii.email)){
+  if (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(window.g_ts__pii.email) ){
+    window.g_ts__config.is_fired = true;
     window.dataLayer.push({ event:window.g_ts__config.user_provided_data_event});
+    setTimeout(function (){ window.g_ts__config.is_fired = false; }, 1000);
   }
 };
-
-document.addEventListener('click',function(){
+window.g_ts__app.dispatchEvent = function(){
+  if (window.g_ts__config.is_fired) return;
   window.g_ts__app.findEmailsStart();
-});
-document.addEventListener('submit',function(){
-  window.g_ts__app.findEmailsStart();
-});
+}
+document.addEventListener('click',window.g_ts__app.dispatchEvent );
+document.addEventListener('submit',window.g_ts__app.dispatchEvent );
 
 if (window.g_ts__app.checkWindowLoaded() ){
-  window.g_ts__app.findEmailsStart();
+  window.g_ts__app.dispatchEvent ();
 }else{
-  window.document('DOMContentLoaded', function(){
-    window.g_ts__app.findEmailsStart();
-  });
+  window.document('DOMContentLoaded', window.g_ts__app.dispatchEvent);
 }
